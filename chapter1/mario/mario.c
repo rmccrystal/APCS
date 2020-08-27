9ec01dd46294384fe69f98e5ba207068f738d12d
Ryan McCrystal <rrmccrystal@gmail.com>
2020-08-26 15:34:31 -1000

    Create mario.c

---
+++ chapter1/mario/mario.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void generateMarioTower(int height);
+
+int main(void)
+{
+    int height;
+    while (true) {
+        height = get_int("Height: ");
+        if(height > 0 && height <= 8)
+            break;
+    }
+    generateMarioTower(height);
+}
+
+// Prints some text x times without adding a newline
+void repeatPrint(const char *text, int times)
+{
+    for (int i = 0; i < times; i++)
+    {
+        printf("%s", text);
+    }
+}
+
+void generateMarioTower(int height)
+{
+    for (int i = 1; i <= height; i++)
+    {
+        repeatPrint(" ", height - i);
+        repeatPrint("#", i);
+        printf("  ");
+        repeatPrint("#", i);
+        printf("\n");
+    }
+}
